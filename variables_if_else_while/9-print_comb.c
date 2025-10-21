46ca393ef973e8919b588e7aa5ab19b63db00301
Gwendal-B <12309@holbertonstudents.com>
2025-10-21 10:04:19 +0000

    add 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int single_digit;
+
+	for (single_digit = 0, single_digit <= 9, single_digit++)
+	{
+		putchar(single_digit + '0');
+
+		if (signle_digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

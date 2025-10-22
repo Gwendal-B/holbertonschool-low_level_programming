23af81429ca5b15a8e9c1b4d69aff0b8c2a72341
Gwendal-B <12309@holbertonstudents.com>
2025-10-22 08:25:00 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - affiche l'alphabet en minuscules suivi d'un saut de ligne
+ * print_alphabet_x10 - affiche l'alphabet en minuscules 10 fois suivi
+ *d'un saut de ligne
  */
 void print_alphabet_x10(void)
 {
-	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+	char lettre;
 	int i;
 
-	for (i = 0; i> 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-
-		int j = 0;
-		while (alphabet[j] != '\0')
+		for (lettre = 'a'; lettre <= 'z'; lettre++)
 		{
-			_putchar(alphabet[j]);
-			j++;
+			_putchar(lettre);
 		}
 		_putchar('\n');
 	}

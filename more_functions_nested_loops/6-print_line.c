4f1c33259c4039142c8761c927d67cdde36da44f
Gwendal-B <12309@holbertonstudents.com>
2025-10-27 10:57:36 +0000

    add 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - fait une ligne
+ *@n: longueur de la ligne
+ *
+ *Return: R
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

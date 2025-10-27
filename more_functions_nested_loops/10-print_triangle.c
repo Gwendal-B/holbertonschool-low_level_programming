efd398f0e90dc96a12c66fde835e3da528d61388
Gwendal-B <12309@holbertonstudents.com>
2025-10-27 13:44:27 +0000

    add 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_triangle - dessine un triangle avec #
+ *@size: taille du triangle
+ *
+ *Return: R
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size -i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

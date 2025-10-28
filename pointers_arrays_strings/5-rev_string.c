13332bf0aa580bc65a6c570ddf4d82b720d1ed55
Gwendal-B <12309@holbertonstudents.com>
2025-10-28 12:36:26 +0000

    add 2 My School (1 normal - 1 reverse)

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - inverse la chaine de caractere
+ *@s: pointe la chaine de caractere
+ *
+ *Return: R
+ */
+void rev_string(char *s)
+{
+	int i, len = 0;
+	char temporaire;
+
+	while (s[len] != 0)
+	{
+		len++;
+	}
+	for ( i = 0; i < len / 2; i++)
+	{
+		temporaire = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temporaire;
+	}
+}

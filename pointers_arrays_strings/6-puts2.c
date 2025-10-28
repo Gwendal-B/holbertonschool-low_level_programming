b6028037d769cd7b6f28721c45bcdcf6b3d58f09
Gwendal-B <12309@holbertonstudents.com>
2025-10-28 14:24:13 +0000

    add nombre pair

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - affiche un caracter sur 2 d'un chaine
+ *@str: pointeur vers la chaine de caractere
+ *
+ *Return: R
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (*str)
+	{
+		if (a == 0)
+			_putchar(*str);
+		a = 1 -a;
+		str++;
+	}
+	_putchar('\n');
+}

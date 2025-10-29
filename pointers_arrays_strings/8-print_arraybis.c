cb77a9fedf919b85bdacf35172f61f7b8875854d
Gwendal-B <12309@holbertonstudents.com>
2025-10-29 08:32:18 +0000

    ajout du 8 bis

---
+++ pointers_arrays_strings/8-print_arraybis.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - affiche n element du tableau
+ *@a: pointe vers le premier element du tableau
+ *@n: nombre d'element a afficher
+ *
+ *Return: R
+ */
+void print_array(int *a, int n)
+{
+	while (n--)
+	{
+		printf("%d", *a);
+		a++;
+
+		if (n)
+			printf(", ");
+	}
+	printf("\n");
+}

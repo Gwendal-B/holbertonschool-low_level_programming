ac71deca0e6b097e643e4d51de83cf17bb4086f1
Gwendal-B <12309@holbertonstudents.com>
2025-11-03 14:57:22 +0000

    ajout de 3-strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+	int match;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		match = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
+		}
+		if (match == 0)
+			break;
+		count++;
+	}
+	return (count);
+}

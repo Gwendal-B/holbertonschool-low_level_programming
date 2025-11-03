a4d4a6e68352e122ee69df7da1718c8aed521f6f
Gwendal-B <12309@holbertonstudents.com>
2025-11-03 14:12:52 +0000

    ajout de strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strchr - localise un caractere dans une chaine
+ *@s: la chaine dans laquelle chercher
+ *@c: le caractere a trouver
+ *
+ *Return: pointeur vers la premiere occurence de c, ou NULL si non trouver
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}

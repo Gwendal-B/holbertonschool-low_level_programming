378664ceb96a3fa133ff699be6430886f5180828
Gwendal-B <12309@holbertonstudents.com>
2025-11-04 11:12:47 +0000

    ajout du strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - cherche le premier caractère de s pesent dans accept
+ * @s: chain a  parcourir
+ * @accept: ensemble de caractère a chercher
+ *
+ * Return: pointeur vers le premier caractere correspondant, ou NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}

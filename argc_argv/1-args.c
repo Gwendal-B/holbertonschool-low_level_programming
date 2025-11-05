9a97572c044be02b2f2790fd6a4d34973bb35484
Gwendal-B <12309@holbertonstudents.com>
2025-11-05 20:37:32 +0000

    correction du args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - affiche le nombre d'arguments passés au programme
- *@argc: nombre total d'arguments
- *@argv: tableau contenant les arguments
+ * main - affiche le nombre d'arguments passés au programme
+ * @argc: nombre total d'arguments
+ * @argv: tableau contenant les arguments
  *
- *Return: 0 toujours
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	int count = argc - 1;
+
 	(void) argv;
-	
-	printf("%d\n", argc - 1);
+	printf("%d\n", count);
+
 	return (0);
 }
+

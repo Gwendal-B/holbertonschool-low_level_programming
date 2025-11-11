5c43575f370d432fa7185fe12234c50041d24f7e
Gwendal-B <12309@holbertonstudents.com>
2025-11-11 12:45:56 +0100

    ajout de malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloue de la memoire et quitte en cas d'echec.
+ * @b: taille du bloc a allouer en octets.
+ *
+ * Return: pointeur vers le bloc memoire alloué
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);
+
+    return (ptr);
+}

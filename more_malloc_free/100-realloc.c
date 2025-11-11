28b3bb01f89c84596f342392b8132642c38ca43c
Gwendal-B <12309@holbertonstudents.com>
2025-11-11 18:49:22 +0000

    full compil plus full check

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * _realloc - réalloue une zone mémoire
  * @ptr: pointeur vers la zone mémoire de base
  * @old_size: taille de la zone mémoire de base
  * @new_size: taille de la nouvelle zone mémoire
- * 
+ *
  * Return: pointeur vers la nouvelle zone mémoire ou NULL en cas d'échec
  */
 
@@ -19,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
     if (new_size == old_size)
         return (ptr);
-    
+
     if (new_size == 0 && ptr != NULL)
     {
         free(ptr);
@@ -28,11 +29,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
     if (ptr == NULL)
         return (malloc(new_size));
-    
+
     new_ptr = malloc(new_size);
     if (new_ptr == NULL)
         return (NULL);
-    
+
     old = ptr;
     new = new_ptr;
     for (i = 0; i < old_size && i < new_size; i++)

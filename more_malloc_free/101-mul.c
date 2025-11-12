0f85c03d7d41e660a24d985abdd74c03d043f4e9
Gwendal-B <12309@holbertonstudents.com>
2025-11-12 13:50:30 +0100

    fix typo in multiplication function

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -44,9 +44,11 @@ int main(int argc, char *argv[])
 	int len1, len2, *result, i, j, carry, n1, n2, sum;
 
 	if (argc != 3 || !is_digit_str(num1) || !is_digit_str(num2))
-		return (print_error());
+		print_error();
 	num1 = argv[1];
 	num2 = argv[2];
+	if (!is_digit_str(num1) || !is_digit_str(num2))
+		print_error();
 	len1 = strlen(num1);
 	len2 = strlen(num2);
 	result = calloc(len1 + len2, sizeof(int));
@@ -55,8 +57,7 @@ int main(int argc, char *argv[])
 	for (i = len1 - 1; i >= 0; i--)/*Multiplication des chiffres*/
 	{
 		n1 = num1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
+		for (carry = j = len2 - 1; j >= 0; j--)
 		{
 			n2 = num2[j] - '0';
 			sum = n1 * n2 + result[i + j + 1] + carry;
@@ -65,9 +66,9 @@ int main(int argc, char *argv[])
 		}
 		result[i + j + 1] += carry;
 	}
-	i = 0;/*Affichage du résultat*/
-	while (i < len1 + len2 && result[i] == 0)
-		i++; /*Ignorer les zéros de tête*/
+	/*Affichage du résultat*/
+	for (i = 0; i < len1 + len2 && result[i] == 0; i++)
+		; /*Ignorer les zéros de tête*/
 	if (i == len1 + len2) /* résultat = 0 */
 		_putchar('0');
 	else

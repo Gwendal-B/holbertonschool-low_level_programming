4135ce16be2c3888dd7a230d7b2d0026dfd780b5
Gwendal-B <12309@holbertonstudents.com>
2025-11-12 23:06:30 +0000

    c'est long la

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -6,7 +6,7 @@ int trouver_longueur(char *str);
 char *creer_tableau_x(int size);
 char *ignorer_zero_initiaux(char *str);
 void obtenir_produit(char *produit, char *mult, int digit, int zeroes);
-void ajouter_nombres(char *produit_final, char *produit_suivant, int taille_suivant);
+void ajouter_nombres(char *produit_final, char *produit_suiv, int taille_suiv);
 
 /**
  * trouver_longueur - Trouve la longueur d'une chaîne.
@@ -16,47 +16,48 @@ void ajouter_nombres(char *produit_final, char *produit_suivant, int taille_suiv
  */
 int trouver_longueur(char *str)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*str++)
-        len++;
+	while (*str++)
+		len++;
 
-    return (len);
+	return (len);
 }
 
 /**
- * creer_tableau - Crée un tableau de caractères initialisé 
+ * creer_tableau_x - Crée un tableau de caractères initial'x'.
  * @size: taille du tableau
  *
  * Return: pointeur vers le tableau
  */
 char *creer_tableau_x(int size)
 {
-    char *tab;
-    int i;
+	char *tab;
+	int i;
 
-    tab = malloc(sizeof(char) * size);
-    if (tab == NULL)
-        exit(98);
+	tab = malloc(sizeof(char) * size);
+	if (tab == NULL)
+		exit(98);
 
-    for (i = 0; i < size - 1; i++)
-        tab[i] = 'x';
-    tab[i] = '\0';
+	for (i = 0; i < size - 1; i++)
+		tab[i] = 'x';
+	tab[i] = '\0';
 
-    return (tab);
+	return (tab);
 }
 
 /**
- * ignorer_zero_initiaux - Avance dans la chaîne jusqu'au premier chiffre non nul
+ * ignorer_zero_initiaux - Avance dans la chaîne jusqu'au premier
+ *  chiffre non nul
  * @str: chaîne de chiffres
  *
  * Return: pointeur sur le premier chiffre non nul
  */
 char *ignorer_zero_initiaux(char *str)
 {
-    while (*str && *str == '0')
-        str++;
-    return (str);
+	while (*str && *str == '0')
+		str++;
+	return (str);
 }
 
 /**
@@ -67,93 +68,94 @@ char *ignorer_zero_initiaux(char *str)
  */
 int obtenir_chiffre(char c)
 {
-    int digit = c - '0';
-    if (digit < 0 || digit > 9)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    return (digit);
+	int digit = c - '0';
+
+	if (digit < 0 || digit > 9)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (digit);
 }
 
 /**
  * obtenir_produit - Multiplie une chaîne de nombres par un chiffre
  * @produit: buffer pour le résultat
  * @mult: chaîne de nombres
- * @chiffre: chiffre unique
+ * @digit: chiffre unique
  * @zeroes: nombre de zéros initiaux
  */
 void obtenir_produit(char *produit, char *mult, int digit, int zeroes)
 {
-    int len_mult, num, retenue = 0;
+	int len_mult, num, retenue = 0;
 
-    len_mult = trouver_longueur(mult) - 1;
-    mult += len_mult;
+	len_mult = trouver_longueur(mult) - 1;
+	mult += len_mult;
 
-    while (*produit)
-    {
-        *produit = 'x';
-        produit++;
-    }
-    produit--;
+	while (*produit)
+	{
+		*produit = 'x';
+		produit++;
+	}
+	produit--;
 
-    while (zeroes--)
-    {
-        *produit = '0';
-        produit--;
-    }
+	while (zeroes--)
+	{
+		*produit = '0';
+		produit--;
+	}
 
-    for (; len_mult >= 0; len_mult--, mult--, produit--)
-    {
-        if (*mult < '0' || *mult > '9')
-        {
-            printf("Error\n");
-            exit(98);
-        }
-        num = (*mult - '0') * digit + retenue;
-        *produit = (num % 10) + '0';
-        retenue = num / 10;
-    }
+	for (; len_mult >= 0; len_mult--, mult--, produit--)
+	{
+		if (*mult < '0' || *mult > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		num = (*mult - '0') * digit + retenue;
+		*produit = (num % 10) + '0';
+		retenue = num / 10;
+	}
 
-    if (retenue)
-        *produit = (retenue % 10) + '0';
+	if (retenue)
+		*produit = (retenue % 10) + '0';
 }
 
 /**
  * ajouter_nombres - Additionne deux tableaux représentant des nombres
  * @produit_final: résultat cumulatif
- * @produit_suivant: prochain produit à ajouter
- * @taille_suivant: taille du prochain produit
+ * @produit_suiv: prochain produit à ajouter
+ * @taille_suiv: taille du prochain produit
  */
-void ajouter_nombres(char *produit_final, char *produit_suivant, int taille_suivant)
+void ajouter_nombres(char *produit_final, char *produit_suiv, int taille_suiv)
 {
-    int num, retenue = 0;
+	int num, retenue = 0;
 
-    while (*(produit_final + 1))
-        produit_final++;
-    while (*(produit_suivant + 1))
-        produit_suivant++;
+	while (*(produit_final + 1))
+		produit_final++;
+	while (*(produit_suiv + 1))
+		produit_suiv++;
 
-    for (; *produit_final != 'x'; produit_final--)
-    {
-        num = (*produit_final - '0') + (*produit_suivant - '0') + retenue;
-        *produit_final = (num % 10) + '0';
-        retenue = num / 10;
-        produit_suivant--;
-        taille_suivant--;
-    }
+	for (; *produit_final != 'x'; produit_final--)
+	{
+		num = (*produit_final - '0') + (*produit_suiv - '0') + retenue;
+		*produit_final = (num % 10) + '0';
+		retenue = num / 10;
+		produit_suiv--;
+		taille_suiv--;
+	}
 
-    for (; taille_suivant >= 0 && *produit_suivant != 'x'; taille_suivant--)
-    {
-        num = (*produit_suivant - '0') + retenue;
-        *produit_final = (num % 10) + '0';
-        retenue = num / 10;
-        produit_final--;
-        produit_suivant--;
-    }
+	for (; taille_suiv >= 0 && *produit_suiv != 'x'; taille_suiv--)
+	{
+		num = (*produit_suiv - '0') + retenue;
+		*produit_final = (num % 10) + '0';
+		retenue = num / 10;
+		produit_final--;
+		produit_suiv--;
+	}
 
-    if (retenue)
-        *produit_final = (retenue % 10) + '0';
+	if (retenue)
+		*produit_final = (retenue % 10) + '0';
 }
 
 /**
@@ -165,45 +167,45 @@ void ajouter_nombres(char *produit_final, char *produit_suivant, int taille_suiv
  */
 int main(int argc, char *argv[])
 {
-    char *produit_final, *produit_suivant;
-    int size, index, digit, zeroes = 0;
+	char *produit_final, *produit_suiv;
+	int size, index, digit, zeroes = 0;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    if (*(argv[1]) == '0')
-        argv[1] = ignorer_zero_initiaux(argv[1]);
-    if (*(argv[2]) == '0')
-        argv[2] = ignorer_zero_initiaux(argv[2]);
-    if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (*(argv[1]) == '0')
+		argv[1] = ignorer_zero_initiaux(argv[1]);
+	if (*(argv[2]) == '0')
+		argv[2] = ignorer_zero_initiaux(argv[2]);
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    size = trouver_longueur(argv[1]) + trouver_longueur(argv[2]);
-    produit_final = creer_tableau_x(size + 1);
-    produit_suivant = creer_tableau_x(size + 1);
+	size = trouver_longueur(argv[1]) + trouver_longueur(argv[2]);
+	produit_final = creer_tableau_x(size + 1);
+	produit_suiv = creer_tableau_x(size + 1);
 
-    for (index = trouver_longueur(argv[2]) - 1; index >= 0; index--)
-    {
-        digit = obtenir_chiffre(*(argv[2] + index));
-        obtenir_produit(produit_suivant, argv[1], digit, zeroes++);
-        ajouter_nombres(produit_final, produit_suivant, size - 1);
-    }
+	for (index = trouver_longueur(argv[2]) - 1; index >= 0; index--)
+	{
+		digit = obtenir_chiffre(*(argv[2] + index));
+		obtenir_produit(produit_suiv, argv[1], digit, zeroes++);
+		ajouter_nombres(produit_final, produit_suiv, size - 1);
+	}
 
-    for (index = 0; produit_final[index]; index++)
-    {
-        if (produit_final[index] != 'x')
-            putchar(produit_final[index]);
-    }
-    putchar('\n');
+	for (index = 0; produit_final[index]; index++)
+	{
+		if (produit_final[index] != 'x')
+			putchar(produit_final[index]);
+	}
+	putchar('\n');
 
-    free(produit_suivant);
-    free(produit_final);
+	free(produit_suiv);
+	free(produit_final);
 
-    return (0);
+	return (0);
 }

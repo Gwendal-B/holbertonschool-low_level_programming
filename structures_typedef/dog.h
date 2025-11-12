9699a854ecd894bf1e09faaf0ab7cfc594368610
Gwendal-B <12309@holbertonstudents.com>
2025-11-12 14:05:20 +0100

    ajout du dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - A structure representant un chien
+* @name: nom du chien
+* @age: age du chien
+* @owner: proprietaire du chien
+*
+* Description: Cette structure contient des informations sur un chien,
+*y compris son nom, son age et le nom de son proprietaire.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

963243c9560486acd617085c45bf1ff646088e52
Gwendal-B <12309@holbertonstudents.com>
2025-12-03 14:52:25 +0100

    fix: betty add static fonction

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,16 +39,52 @@ void close_file(int fd)
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
+ * copy_file - Copies content from one file to another.
+ * @fd_from: File descriptor of the source file.
+ * @fd_to: File descriptor of the destination file.
+ * @buf: Buffer to use for copying.
+ * @file_from: Name of the source file (for error message).
+ * @file_to: Name of the destination file (for error message).
+ */
+
+static void copy_file(int fd_from, int fd_to, char *buf, const char *file_from,
+                      const char *file_to)
+{
+    ssize_t r, w;
+
+    while ((r = read(fd_from, buf, 1024)) > 0)
+    {
+        w = write(fd_to, buf, r);
+        if (w != r)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+            free(buf);
+            close_file(fd_from);
+            close_file(fd_to);
+            exit(99);
+        }
+    }
+
+    if (r == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        free(buf);
+        close_file(fd_from);
+        close_file(fd_to);
+        exit(98);
+    }
+}
+
+/**
+ * main - Entry point for the cp program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
- * Return: 0 on success.
+ * Return: 0 on success, exits with various codes on failure.
  */
 int main(int argc, char *argv[])
 {
     int fd_from, fd_to;
-    ssize_t r, w;
     char *buffer;
 
     if (argc != 3)
@@ -75,31 +111,7 @@ int main(int argc, char *argv[])
         exit(99);
     }
 
-    /* Boucle de copie déplacée dans une fonction inline anonyme */
-    {
-        char *buf = buffer;
-
-        while ((r = read(fd_from, buf, 1024)) > 0)
-        {
-            w = write(fd_to, buf, r);
-            if (w != r)
-            {
-                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                free(buffer);
-                close_file(fd_from);
-                close_file(fd_to);
-                exit(99);
-            }
-        }
-        if (r == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-            free(buffer);
-            close_file(fd_from);
-            close_file(fd_to);
-            exit(98);
-        }
-    }
+    copy_file(fd_from, fd_to, buffer, argv[1], argv[2]);
 
     free(buffer);
     close_file(fd_from);

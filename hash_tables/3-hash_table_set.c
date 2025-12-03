f5fc543134506a7d7edac90cb154222127b45807
Gwendal-B <12309@holbertonstudents.com>
2025-12-03 19:12:38 +0100

    add many task (2-key_index/3-hash_tables_set/4-hash_table_get

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - Adds or updates an element in the hash table
+ * @ht: the hash table
+ * @key: the key (string, cannot be empty)
+ * @value: the value (string, duplicated)
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *tmp;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	/* If key exists -> update value */
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	/* Key does not exist -> create new node */
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}

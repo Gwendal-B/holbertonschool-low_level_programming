d18772d587a74367b8b5f3afe3b19de4ca76b1f7
Gwendal-B <12309@holbertonstudents.com>
2025-12-04 21:12:43 +0100

    fix: checker

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,32 +8,32 @@
  */
 hash_node_t *create_node(const char *key, const char *value)
 {
-    hash_node_t *new_node;
+	hash_node_t *new_node;
 
-    if (!key || strlen(key) == 0 || !value)
-        return (NULL);
+	if (!key || strlen(key) == 0 || !value)
+		return (NULL);
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->key = strdup(key);
-    if (!new_node->key)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->value = strdup(value);
-    if (!new_node->value)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (NULL);
-    }
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = NULL;
-    return (new_node);
+	new_node->next = NULL;
+	return (new_node);
 }
 
 /**
@@ -45,34 +45,34 @@ hash_node_t *create_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *tmp, *new_node;
+	unsigned long int index;
+	hash_node_t *tmp, *new_node;
 
-    if (!ht || !key || strlen(key) == 0 || !value)
-        return (0);
+	if (!ht || !key || strlen(key) == 0 || !value)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    tmp = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
 
-    /* Update value if key already exists */
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            return (tmp->value ? 1 : 0);
-        }
-        tmp = tmp->next;
-    }
+	/* Update value if key already exists */
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (tmp->value ? 1 : 0);
+		}
+		tmp = tmp->next;
+	}
 
-    /* Key does not exist, create a new node */
-    new_node = create_node(key, value);
-    if (!new_node)
-        return (0);
+	/* Key does not exist, create a new node */
+	new_node = create_node(key, value);
+	if (!new_node)
+		return (0);
 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    return (1);
+	return (1);
 }
